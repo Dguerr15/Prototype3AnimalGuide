1afda39b9bc245b2d66cde1262ea39354ccb4a6c
alaaddingh <alagho03@gmail.com>
2025-10-30 12:24:01 -0700

    spike node + instances added(+ future tileset for spikes in 'assets')

---
+++ guide-animal/spike.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Animal"):
+		call_deferred("_reload_scene")
+
+func _reload_scene() -> void:
+	get_tree().reload_current_scene() 
